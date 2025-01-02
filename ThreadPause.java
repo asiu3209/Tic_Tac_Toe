23fa8b69f632c6fa1ee45d4734e02923cd4f5584
Aaron Siu <aarons3209@gmail.com>
2025-01-02 18:32:48 -0500

    shortened code

--- ThreadPause.java
+++ ThreadPause.java
@@ -1,9 +1,11 @@
 public class ThreadPause {
         // method to pause the string
         // here we will pass the time in seconds
+        @SuppressWarnings("CallToPrintStackTrace")
         public void wait(int sec) {
             try {
-                Thread.currentThread().sleep(sec * 1000);
+                Thread.currentThread();
+                Thread.sleep(sec * 1000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
